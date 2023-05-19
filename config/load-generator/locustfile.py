783f622aaeba7c8755cadbfe4f537e396876b1f8
Laszlo Igaz <laszlo.igaz.epam+ah@gmail.com>
2023-05-19 20:05:04 +0200

    Locally the services always get a different IP. Updated

--- config/load-generator/locustfile.py
+++ config/load-generator/locustfile.py
@@ -9,7 +9,7 @@ class QuickstartUser(HttpUser):
     @task
     def getOneFilm(self):
         film_id = random.randint(1, 150)
-        self.client.get(f"http://172.29.233.138:18180/api/v1/films/{film_id}", name="/api/v1/films/[film_id]")
+        self.client.get(f"http://172.20.61.173:18180/api/v1/films/{film_id}", name="/api/v1/films/[film_id]")
 
     @task
     def getFirstFilms(self):
@@ -17,4 +17,4 @@ class QuickstartUser(HttpUser):
 
     @task
     def getFirstActors(self):
-        self.client.get("http://172.29.233.138:18181/api/v1/actors")
+        self.client.get("http://172.29.233.138:18180/api/v1/actors")
